51f00846d545ff75cd661e97ab65610aa1033c9c
Mike <mike@Mikes-Mac-mini.local>
2022-04-27 21:56:33 +0700

    Update small style

--- src/woo/components/PreviewImage.js
+++ src/woo/components/PreviewImage.js
@@ -1,5 +1,4 @@
-import React, { Fragment } from 'react';
-import PreviewImage from '../../../images/preview-image.png';
+import React from 'react';
 import styled from 'styled-components'; 
 
 const PreviewImageContainer = styled.div`
@@ -13,6 +12,7 @@ const JacketCoverImageContainer = styled.div`
   background-color: black;
   background-image: ${ props => (props.coverimage ? `url(${ props.coverimage })` : 'none') };
   background-size: cover;
+  background-position: center center;
   box-shadow: 0 0 10px 0 rgb(1 1 1 / 20%), 25px 0 0 0 #f5f5f5;
 `
 
@@ -27,6 +27,5 @@ export const Record = ({ style, colour }) => {
 export default ({ variant, jacketcover, sides }) => {
   return <PreviewImageContainer>
     <JacketCoverImageContainer coverimage={ jacketcover } />
-    {/* <img src={PreviewImage } alt="#" /> */}
   </PreviewImageContainer>
 }
