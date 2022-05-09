153ea82ea5155a8c88a40bb60d2c66d45aaab6d8
Mike.tropi <mike.beplus@gmail.com>
2022-05-09 16:28:11 +0700

    Update My Account page

--- inc/helpers.php
+++ inc/helpers.php
@@ -3,3 +3,13 @@
 function clampdown_load_header() {
   load_template(CLAMPDOWN_DIR . '/templates/header-default.php', false);
 }
+
+/**
+ * @param String $name
+ * 
+ * @return Html SVG
+ */
+function clampdown_svg($name) {
+  $icons = require(__DIR__ . '/svg.php'); 
+  return ($icons[$name]) ? $icons[$name] : '';
+}
