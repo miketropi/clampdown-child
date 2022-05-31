1b4a38009838d63c6e3d86569b62b7ae5ba207e7
Mike.tropi <mike.beplus@gmail.com>
2022-05-31 14:53:41 +0700

    Update acf opts

--- inc/options.php
+++ inc/options.php
@@ -1301,6 +1301,50 @@ if( function_exists('acf_add_local_field_group') ):
 	));
 	
 	acf_add_local_field_group(array(
+		'key' => 'group_6295bce4c62ae',
+		'title' => 'Order Attachments Settings',
+		'fields' => array(
+			array(
+				'key' => 'field_6295bd776b9d4',
+				'label' => 'Select File Attachments',
+				'name' => 'clampdown_select_file_attachments',
+				'type' => 'file',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'return_format' => 'array',
+				'library' => 'all',
+				'min_size' => '',
+				'max_size' => '',
+				'mime_types' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'shop_order',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'side',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'field',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	));
+	
+	acf_add_local_field_group(array(
 		'key' => 'group_624e49448630a',
 		'title' => 'Request Quote Settings',
 		'fields' => array(
